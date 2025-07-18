b572488401195742ca53b011dd7418a78dc501a6
Anastasiia Rubanova <anastasiia.rubanova.pm.2021@lpnu.ua>
2025-07-18 07:58:51 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  base: '/GOIT-JS-HW-09/',
   build: {
     outDir: '../dist',
+    emptyOutDir: true,  // ← додаємо сюди також
   },
   define: {
     global: 'window',
   },
 });
+
